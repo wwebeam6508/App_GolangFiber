fadfe4c081365ff97b4ad1b56b30cbf67cd902de
Wim <policebeam@gmail.com>
2024-01-27 04:03:05 +0700

    add configto route

--- routes/route.go
+++ routes/route.go
@@ -5,10 +5,18 @@ import (
 	// import other routes...
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func SetupRoutes(app *fiber.App) {
-
+	f := fiber.New()
+	f.Use(logger.New())
+	f.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+		AllowMethods: "GET, POST, PUT, DELETE",
+	}))
 	AuthGroup := app.Group("/api")
 	AuthGroup.Route("/auth", AuthRoute)
 }
